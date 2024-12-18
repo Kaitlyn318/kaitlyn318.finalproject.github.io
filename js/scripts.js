997d888791b1f1652c5839533db3243ce5276a08
Kaitlyn318 <kbro318@byu.edu>
2024-12-18 09:14:26 -0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*!
+* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+const solution = "photo"; // Set the solution word
+const maxGuesses = 6;
+let currentRow = 0;
+let currentGuess = "";
+
+
